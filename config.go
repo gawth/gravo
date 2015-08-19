543965c6c7f6a588c29f75f1c274b60361346961
Alan Gawthorpe <agawthorpe@gmail.com>
2015-08-19 08:18:56 +0100

    Move config out to sep file

---
+++ config.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+)
+
+var configFile = "gravo.yml"
+
+type config struct {
+	Host string
+	Port string
+	Path string
+}
+
+func readConfigFile(file string) []byte {
+	f, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal("error: %v", err)
+	}
+	return f
+}
+
+func convertYaml(raw []byte) config {
+	c := config{}
+
+	err := yaml.Unmarshal(raw, &c)
+	if err != nil {
+		log.Fatal("error: %v", err)
+	}
+	return c
+}
+func LoadConfig(file string) config {
+	return convertYaml(readConfigFile(file))
+}
--- gravo.go
+++ gravo.go
@@ -2,41 +2,11 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 )
-
-var configFile = "gravo.yml"
-
-type config struct {
-	Host string
-	Port string
-	Path string
-}
-
-func readConfigFile(file string) []byte {
-	f, err := ioutil.ReadFile(file)
-	if err != nil {
-		log.Fatal("error: %v", err)
-	}
-	return f
-}
-
-func convertYaml(raw []byte) config {
-	c := config{}
-
-	err := yaml.Unmarshal(raw, &c)
-	if err != nil {
-		log.Fatal("error: %v", err)
-	}
-	return c
-}
-func LoadConfig(file string) config {
-	return convertYaml(readConfigFile(file))
-}
 
 func main() {
 
