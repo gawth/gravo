8e58699ff0f7c5e550986ea25c645cd9a003d5f8
Alan Gawthorpe <agawthorpe@gmail.com>
2015-12-11 15:38:11 +0000

    Additional output formatting
    Just tinkered with format of numbers being spat out by the stdout output
    handler

--- outputhandler.go
+++ outputhandler.go
@@ -19,7 +19,7 @@ func (so *standardOutput) DealWithIt(r http.Response, t Timer) {
 		log.Println(err)
 		return
 	}
-	log.Println(fmt.Sprintf(",%d,%d,%v\n", len(payload), len(payload)/1024/1024, t.GetTime()))
+	log.Println(fmt.Sprintf(", %db, %.5fmb, %v", len(payload), float64(len(payload))/1024/1024, t.GetTime()))
 	if so.Verbose {
 		fmt.Fprintln(os.Stderr, string(payload))
 	}
@@ -33,5 +33,5 @@ func (so *standardOutput) LogInfo(s string) {
 }
 
 func (so *standardOutput) Start() {
-	fmt.Println("timestamp,bytes, meg, duration")
+	fmt.Println("timestamp, bytes, meg, duration")
 }
