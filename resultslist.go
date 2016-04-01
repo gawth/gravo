6e7c8d0ca0752105fc58d16cf60810af39f6df51
Alan Gawthorpe <agawthorpe@gmail.com>
2016-04-01 15:59:16 +0100

    Added support for list of results from file system

--- resultslist.go
+++ resultslist.go
@@ -1,15 +1,42 @@
 package main
 
-import ()
+import (
+	"io/ioutil"
+	"log"
+)
 
 type ResultsList struct {
 	liveResults Results
+	folder      string
+	filelist    []string
 }
 
 func (rl *ResultsList) GetList() []string {
-	return []string{rl.liveResults.Name()}
+	tmp := []string{rl.liveResults.Name()}
+	tmp = append(tmp, rl.filelist...)
+	return tmp
 }
 
-func NewResultsList(liveResults Results) ResultsList {
-	return ResultsList{liveResults: liveResults}
+func (rl *ResultsList) GetResults(name string) string {
+	return ""
+}
+func (rl *ResultsList) InitResultsList() {
+	if len(rl.folder) == 0 {
+		return
+	}
+	files, err := ioutil.ReadDir(rl.folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		if !file.IsDir() {
+			rl.filelist = append(rl.filelist, file.Name())
+		}
+	}
+}
+
+func NewResultsList(liveResults Results, folder string) ResultsList {
+	res := ResultsList{liveResults: liveResults, folder: folder}
+	res.InitResultsList()
+	return res
 }
--- resultslist_test.go
+++ resultslist_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestResultsListGetList(t *testing.T) {
-	target := NewResultsList(NewResults("fred"))
+	target := NewResultsList(NewResults("fred"), "")
 
 	list := target.GetList()
 
@@ -24,6 +24,9 @@ func setUpTestFiles() {
 		os.Create(folder + "/" + name)
 	}
 }
+func addFolder() {
+	os.Mkdir(folder+"/another", 0777)
+}
 func destroyTestFiles() {
 	os.RemoveAll(folder)
 }
@@ -32,4 +35,27 @@ func TestResultsListReadFile(t *testing.T) {
 	setUpTestFiles()
 	defer destroyTestFiles()
 
+	target := NewResultsList(Results{}, folder)
+
+	list := target.GetList()
+
+	if len(list) != len(files)+1 {
+		t.Errorf("TestResultsListReadFile: Expected GetList to return %v not %v",
+			len(files)+1, len(list))
+	}
+}
+
+func TestResultsListReadFileWithFolder(t *testing.T) {
+	setUpTestFiles()
+	addFolder()
+	defer destroyTestFiles()
+
+	target := NewResultsList(Results{}, folder)
+
+	list := target.GetList()
+
+	if len(list) != len(files)+1 {
+		t.Errorf("TestResultsListReadFileWithFolder: Expected GetList to return %v not %v",
+			len(files)+1, len(list))
+	}
 }
